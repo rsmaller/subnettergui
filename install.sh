ca4771e9cd5dea4417fcbc8c4720ef03630bf7b6
Ryan <ryjobobo@gmail.com>
2025-03-22 02:03:14 -0400

    Fixed window icon issues on Linux and the icon for installation in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir ~/.local/share/icons/subnettergui
-cp ./install/subnettergui.svg ~/.local/share/icons/subnettergui/subnettergui.svg
+cp ./install/subnettergui.svg ~/.local/share/icons/subnettergui/subnettergui.svg # Icon by Pexelpy on freeicons.io
 echo "[Desktop Entry]
 Type=Application
 Name=Subnetter++
+StartupWMClass=Subnetter++
 Exec=/usr/local/bin/subnettergui
 Icon=$HOME/.local/share/icons/subnettergui/subnettergui.svg" > ~/.local/share/applications/subnettergui.desktop
 sudo cp ./build/subnettergui /usr/local/bin/subnettergui
